f2f92f6c8bc2a1f3e2064187144f0b7774295856
Adan <adanabdi254@gmail.com>
2026-05-15 15:41:06 -0400

    add Google Analytics tracking code

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,13 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6192423933876456" crossOrigin="anonymous"></script>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-RTVLE9Z5P4"></script>
+        <script dangerouslySetInnerHTML={{ __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-RTVLE9Z5P4');
+        `}} />
       </head>
       <body>{children}</body>
     </html>
