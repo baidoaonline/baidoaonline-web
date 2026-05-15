eb3de9ece98fba0a0f50f2ae3007dee6ae5cbb99
Adan <adanabdi254@gmail.com>
2026-05-15 14:06:00 -0400

    add Google AdSense code

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6192423933876456" crossOrigin="anonymous"></script>
+      </head>
       <body>{children}</body>
     </html>
   );
