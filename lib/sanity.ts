b4cbe945e7e51a1e4dbc483f36d11bb442f4ab1c
Adan <adanabdi254@gmail.com>
2026-06-13 04:05:09 -0400

    fix deprecated sanity image-url import

--- lib/sanity.ts
+++ lib/sanity.ts
@@ -1,5 +1,5 @@
 import { createClient } from 'next-sanity'
-import imageUrlBuilder from '@sanity/image-url'
+import { createImageUrlBuilder as imageUrlBuilder } from '@sanity/image-url'
 
 export const client = createClient({
   projectId: 'bbo1x3xn',
